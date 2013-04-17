392c726dacc4b5bbf9638e409863f8f62564d0f8
Jon Haggblad <jon@haeggblad.com>
2013-04-17 18:26:19 +0100

    Added EnemyStandard

---
+++ src/EnemyStandard.cpp
@@ -0,0 +1,37 @@
+//    Kexx2
+//    Copyright (C) 2001, 2013 Jon Häggblad
+//
+//    This file is part of Kexx2.
+//
+//    Kexx2 is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Kexx2 is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Kexx2.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "EnemyStandard.h"
+#include "FxManager.h"
+#include "ObjectManager.h"
+
+// -----------------------------------------------------------------------------
+// Construction/Destruction
+// -----------------------------------------------------------------------------
+
+EnemyStandard::EnemyStandard(std::string name, int energy, int score, 
+                             sdlc::Surface& s, int init_y_vel)
+    : Object(name, energy, score, s, OBJ_ENEMY, init_y_vel)
+{
+    calculate_hit_img();
+}
+
+// -----------------------------------------------------------------------------
+// Member Functions
+// -----------------------------------------------------------------------------
+
---
+++ src/EnemyStandard.h
@@ -0,0 +1,31 @@
+//    Kexx2
+//    Copyright (C) 2001, 2013 Jon Häggblad
+//
+//    This file is part of Kexx2.
+//
+//    Kexx2 is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Kexx2 is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with Kexx2.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef KEXX2_ENEMYSTANDARD_H
+#define KEXX2_ENEMYSTANDARD_H
+
+#include "Object.h"
+
+class EnemyStandard final : public Object {
+public:
+    EnemyStandard(std::string name, int energy, int score, sdlc::Surface&, 
+                  int init_y_vel);
+    virtual ~EnemyStandard() = default;
+};
+
+#endif // KEXX2_ENEMYSTANDARD_H
