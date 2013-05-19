58386cb0262f752e9f12f6160f7c7e05e54aa214
Jon Haggblad <jon@haeggblad.com>
2013-05-19 20:07:18 +0100

    mingw32 now seems to work as intenden (minus smoke)

--- make_win_dist.sh
+++ make_win_dist.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 mkdir -p /tmp/kexx2
 cp -a src/kexx2.exe /tmp/kexx2
 cp -a data /tmp/kexx2
+cp -a README /tmp/kexx2
+cp -a AUTHORS /tmp/kexx2
+cp -a ChangeLog /tmp/kexx2
+cp -a COPYING /tmp/kexx2
+cp -a INSTALL /tmp/kexx2
+
