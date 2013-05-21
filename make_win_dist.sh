956edcf42e97008f6a275d57b6a770612a1b9a4f
Jon Haggblad <jon@haeggblad.com>
2013-05-21 21:26:51 +0100

    Updated the script to make win dist.

--- make_win_dist.sh
+++ make_win_dist.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-mkdir -p /tmp/kexx2
-cp -a src/kexx2.exe /tmp/kexx2
-cp -a data /tmp/kexx2
-cp -a README /tmp/kexx2
-cp -a AUTHORS /tmp/kexx2
-cp -a ChangeLog /tmp/kexx2
-cp -a COPYING /tmp/kexx2
-cp -a INSTALL /tmp/kexx2
-
+VERSION=$(grep " VERSION " config.h | cut -d' ' -f3 | sed s/\"//g | sed s/\\./_/g)
+DIR=kexx2-$VERSION-win-x86
+mkdir -p $DIR
+cp -a src/kexx2.exe $DIR
+cp -a data $DIR
+cp -a README $DIR/readme.txt
+cp -a AUTHORS $DIR/authors.txt
+cp -a ChangeLog $DIR/changelog.txt
+cp -a COPYING $DIR/copying.txt
+zip -r $DIR.zip $DIR
